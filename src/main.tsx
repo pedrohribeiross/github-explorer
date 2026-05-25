4aae2a54e26e823ffaab5f1406bca5ee36e70ad8
Pedro Henrique <pedro_henrique.rss@hotmail.com>
2026-05-25 17:16:15 -0300

    refactor: remove @tanstack/react-query and related components to simplify data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
-import { queryClient } from './api'
 import { SearchProvider } from './context'
 import { appRouter } from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <SearchProvider>
-        <RouterProvider router={appRouter} />
-      </SearchProvider>
-      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-    </QueryClientProvider>
+    <SearchProvider>
+      <RouterProvider router={appRouter} />
+    </SearchProvider>
   </StrictMode>,
 )
